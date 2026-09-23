75097f281c517c6f27938a7e9cf01a273853a8e9
Abhishek11225 <abhisheksharmaa11225@gmail.com>
2026-09-23 22:28:29 +0530

    Time: 3 ms (88.1%), Space: 71.6 MB (90.18%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,3 +1,28 @@
+// class Solution {
+//     public int numOfSubarrays(int[] arr, int k, int threshold) {
+//         int sum=0;
+//         int count=0;
+//         for(int i=0;i<k;i++){
+//             sum+=arr[i];
+//         }
+//         if(sum/k>=threshold){
+//             count++;
+//         }
+//         for(int j=k;j<arr.length;j++){
+//             sum+=arr[j];
+//             sum-=arr[j-k];
+
+//             if(sum/k>=threshold){
+//             count++;
+//         }
+
+           
+//         }
+//         return count;
+//     }
+// }
+
+
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int sum=0;
@@ -8,16 +33,14 @@ class Solution {
         if(sum/k>=threshold){
             count++;
         }
-        for(int j=k;j<arr.length;j++){
+        for( int j=k;j<arr.length;j++){
             sum+=arr[j];
             sum-=arr[j-k];
 
             if(sum/k>=threshold){
             count++;
         }
-
-           
         }
-        return count;
+        return count ;
     }
 }
