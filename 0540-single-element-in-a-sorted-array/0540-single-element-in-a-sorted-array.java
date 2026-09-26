bf71bf91e9403900a8602c2287f20a440ecd1577
Abhishek11225 <abhisheksharmaa11225@gmail.com>
2026-09-26 16:51:18 +0530

    Time: 0 ms (100%), Space: 52.7 MB (89.33%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,14 +1,45 @@
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         for(int num:nums){
+//             map.put(num,map.getOrDefault(num,0)+1);
+//         }
+//         for(int num:nums){
+//         if(map.get(num)==1){
+//             return num;
+//         }
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashMap<Integer,Integer>map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        int start=0;
+        int end=nums.length-1;
+        
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(mid%2==0){
+
+            
+            if(nums[mid]==nums[mid+1]){
+                start=mid+2;
+            }
+            else{
+                end=mid;
+            }
+            }
+            else{
+                if(nums[mid]==nums[mid-1]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid;
+                }
+            }
         }
-        for(int num:nums){
-        if(map.get(num)==1){
-            return num;
-        }
-        }
-        return -1;
+        return nums[end];
+
     }
 }
