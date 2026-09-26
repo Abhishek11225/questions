80077880abd09527c29737936ee514b45cb0a17d
Abhishek11225 <abhisheksharmaa11225@gmail.com>
2026-09-26 19:59:29 +0530

    Time: 0 ms (100%), Space: 43.8 MB (55.45%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -10,43 +10,43 @@
 // }
 
 
-// class Solution {
-//     public int findMin(int[] nums) {
-//         int start=0;
-//         int end=nums.length-1;
-//         while(start<=end){
-//             int mid=start+(end-start)/2;
-//             if(nums[mid]>nums[end]){
-//                 start=mid+1;
-//             }
-//             else{
-//                 end=mid-1;
-//             }
-            
-            
-//         }
-//         return nums[start];
-//     }
-// }
-
 class Solution {
     public int findMin(int[] nums) {
         int start=0;
         int end=nums.length-1;
-        int ans=Integer.MAX_VALUE;
         while(start<end){
-            
             int mid=start+(end-start)/2;
-            ans=Math.min(ans,nums[mid]);
-
             if(nums[mid]>nums[end]){
                 start=mid+1;
-        }
-        else{
+            }
+            else{
                 end=mid;
             }
+            
+            
+        }
+        return nums[start];
     }
-    ans=Math.min(ans,nums[start]);
-    return ans;
 }
-}
+
+// class Solution {
+//     public int findMin(int[] nums) {
+//         int start=0;
+//         int end=nums.length-1;
+//         int ans=Integer.MAX_VALUE;
+//         while(start<end){
+            
+//             int mid=start+(end-start)/2;
+//             ans=Math.min(ans,nums[mid]);
+
+//             if(nums[mid]>nums[end]){
+//                 start=mid+1;
+//         }
+//         else{
+//                 end=mid;
+//             }
+//     }
+//     ans=Math.min(ans,nums[start]);
+//     return ans;
+// }
+// }
